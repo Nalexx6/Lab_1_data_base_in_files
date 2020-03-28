9a00105d5d109c382e188c08bca3c0ce904dd233
Nalexx6 <60155114+Nalexx6@users.noreply.github.com>
2020-03-28 21:11:54 +0200

    Lab_1(data base in files) vs Lab_3.b(sorting)

---
+++ cmake-build-debug/CMakeFiles/Lab_1_data_base_in_files_.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/Functions.cpp" "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/cmake-build-debug/CMakeFiles/Lab_1_data_base_in_files_.dir/Functions.cpp.obj"
+  "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/Mode.cpp" "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/cmake-build-debug/CMakeFiles/Lab_1_data_base_in_files_.dir/Mode.cpp.obj"
+  "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/Product.cpp" "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/cmake-build-debug/CMakeFiles/Lab_1_data_base_in_files_.dir/Product.cpp.obj"
+  "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/Sorts.cpp" "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/cmake-build-debug/CMakeFiles/Lab_1_data_base_in_files_.dir/Sorts.cpp.obj"
+  "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/main.cpp" "D:/C++/1/1 course 2nd term(2020)/Lab.1(data  base in files)/cmake-build-debug/CMakeFiles/Lab_1_data_base_in_files_.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Lab_1_data_base_in_files_.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_1_data_base_in_files_.dir/Functions.cpp.obj"
+  "CMakeFiles/Lab_1_data_base_in_files_.dir/Mode.cpp.obj"
+  "CMakeFiles/Lab_1_data_base_in_files_.dir/Product.cpp.obj"
+  "CMakeFiles/Lab_1_data_base_in_files_.dir/Sorts.cpp.obj"
+  "CMakeFiles/Lab_1_data_base_in_files_.dir/main.cpp.obj"
+  "Lab_1_data_base_in_files_.exe"
+  "Lab_1_data_base_in_files_.exe.manifest"
+  "Lab_1_data_base_in_files_.pdb"
+  "libLab_1_data_base_in_files_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_1_data_base_in_files_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
